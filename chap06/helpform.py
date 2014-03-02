95ece8c594c7bb99cdbf6e4bc75b70909b18fca2
Jacob su <jacobsu@Jacobs-MacBook-Air.local>
2014-03-02 21:17:17 +0800

    add chap06 example, but still have many bugs to fix

---
+++ chap06/helpform.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# Copyright (c) 2008 Qtrac Ltd. All rights reserved.
+# This program or module is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 2 of the License, or
+# version 3 of the License, or (at your option) any later version. It is
+# provided for educational purposes and is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+# the GNU General Public License for more details.
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from future_builtins import *
+
+from PyQt5.QtCore import (QUrl, Qt)
+from PyQt5.QtWidgets import (QAction, QApplication, QDialog, 
+        QLabel, QTextBrowser, QToolBar, QVBoxLayout)
+from PyQt5.QtGui import QIcon, QKeySequence
+import qrc_resources
+
+
+class HelpForm(QDialog):
+
+    def __init__(self, page, parent=None):
+        super(HelpForm, self).__init__(parent)
+        self.setAttribute(Qt.WA_DeleteOnClose)
+        self.setAttribute(Qt.WA_GroupLeader)
+
+        backAction = QAction(QIcon(":/back.png"), "&Back", self)
+        backAction.setShortcut(QKeySequence.Back)
+        homeAction = QAction(QIcon(":/home.png"), "&Home", self)
+        homeAction.setShortcut("Home")
+        self.pageLabel = QLabel()
+
+        toolBar = QToolBar()
+        toolBar.addAction(backAction)
+        toolBar.addAction(homeAction)
+        toolBar.addWidget(self.pageLabel)
+        self.textBrowser = QTextBrowser()
+
+        layout = QVBoxLayout()
+        layout.addWidget(toolBar)
+        layout.addWidget(self.textBrowser, 1)
+        self.setLayout(layout)
+
+        backAction.triggered.connect(self.textBrowser.backward)
+        homeAction.triggered.connect(self.textBrowser.home)
+        self.textBrowser.sourceChanged.connect(self.updatePageTitle)
+        
+        self.textBrowser.setSearchPaths([":/help"])
+        self.textBrowser.setSource(QUrl(page))
+        self.resize(400, 600)
+        self.setWindowTitle("{0} Help".format(
+                QApplication.applicationName()))
+
+
+    def updatePageTitle(self):
+        self.pageLabel.setText(self.textBrowser.documentTitle())
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    form = HelpForm("index.html")
+    form.show()
+    app.exec_()
+
